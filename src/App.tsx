9e057b7b5bc3bd48dea5e5a6e4cdb770dea3eb4a
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-06-11 12:42:50 -0300

    basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import Question from './components/question/question';
+import QuestionsArray from './questions.json';
+
+function getRandomArbitrary(min: number, max:number) {
+    return Math.random() * (max - min) + min;
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(QuestionsArray[0]) 
+
+  const handleNextQuestion = () => {
+    const randomIndex = Math.floor(getRandomArbitrary(0, QuestionsArray.length))
+    setCurrentQuestion(QuestionsArray[randomIndex])     
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Question
+        text={currentQuestion.text}
+        answers={currentQuestion.answers}
+        correctAnswer={currentQuestion.correctAnswer}
+        handleNextQuestion={handleNextQuestion}
+      />
     </>
   )
 }
---
+++ src/components/question/question.tsx
@@ -0,0 +1,64 @@
+import './question.css'
+
+interface QuestionProps {
+    text: string;
+    answers: string[];
+    correctAnswer: string;
+    handleNextQuestion: () => void;
+}
+
+import React, { useState } from 'react';
+
+function Question({ text, answers, correctAnswer,  handleNextQuestion}: QuestionProps) {
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+    if (selectedAnswer === correctAnswer) {
+        setIsCorrect(true);
+    } else {
+        setIsCorrect(false);
+    }
+    console.log(selectedAnswer);
+  };
+
+  const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>{text}</h1>
+      <form onSubmit={handleSubmit}>
+        <ul id='answers'>
+          {answers.map((answer, index) => (
+            <div key={index} >
+              <input
+                type="radio"
+                id={`answer-${index}`}
+                name="answer"
+                value={answer}
+                checked={selectedAnswer === answer}
+                onChange={handleAnswerChange}
+              />
+              <label id='choicesText' htmlFor={`answer-${index}`}>{answer}</label>
+            </div>
+          ))}
+        </ul>
+              { isSubmitted && <p>Alternativa correta: {correctAnswer}</p> }
+              { isSubmitted && isCorrect && <p>Parabéns, você acertou!</p> }
+              { isSubmitted && !isCorrect && <p>Que pena, você errou!</p> }
+              <div >
+                <button className='buttons' type="submit">Enviar</button>
+                <button className='buttons' type="button" onClick={handleNextQuestion}>Proximo</button>
+            </div>
+      </form>
+    </div>
+  );
+}
+
+export default Question;
+
